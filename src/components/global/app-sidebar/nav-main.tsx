43a1299d5955ef5fa8965a14ed39eb3ae7d433d1
Furkan Balcı <furkan09987@gmail.com>
2025-04-19 15:04:57 +0300

    fix: tailwind ve sidebar fixlendi

--- src/components/global/app-sidebar/nav-main.tsx
+++ src/components/global/app-sidebar/nav-main.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   SidebarGroup,
@@ -6,14 +6,16 @@ import {
   SidebarMenuButton,
   SidebarProvider,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import Link from 'next/link'
+} from "@/components/ui/sidebar";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { HomeIcon, FileTextIcon, TrashIcon, SettingsIcon } from "lucide-react"; // Import icons
 
 // Map icon names to components
-const iconComponents: { [key: string]: React.FC<React.SVGProps<SVGSVGElement>> } = {
+const iconComponents: {
+  [key: string]: React.FC<React.SVGProps<SVGSVGElement>>;
+} = {
   Home: HomeIcon,
   FileText: FileTextIcon,
   Trash: TrashIcon,
@@ -26,44 +28,47 @@ const NavMain = ({
   items: {
     title: string;
     url: string;
-    iconName: string; 
+    iconName: string;
     isActive?: boolean;
     items?: {
       title: string;
-      url: string
-    }[]
-  }[]
+      url: string;
+    }[];
+  }[];
 }) => {
   const pathname = usePathname();
 
   return (
-    <SidebarGroup className="p-0">
-      <SidebarMenu>
-        {items.map((item) => {
-          const IconComponent = iconComponents[item.iconName]; // Get the component from the map
-          return (
-            <SidebarMenuItem key={item.title}>
-              <SidebarProvider>
-              <SidebarMenuButton
-                asChild
-            tooltip={'item.title'}
-            className={`${pathname.includes(item.url) && 'bg-muted'}`}
-          >
-            <Link
-              href={item.url}
-              className={`text-lg ${pathname.includes(item.url) && "font-bold"}`}
-            >
-              {IconComponent && <IconComponent className="text-lg" />} {/* Render the mapped component */}
-              <span>{item.title}</span>
-            </Link>
-          </SidebarMenuButton>
-          </SidebarProvider>
-        </SidebarMenuItem>
-          );
-        })}
-      </SidebarMenu>
-    </SidebarGroup>
-  )
-}
+    <SidebarProvider>
+      <SidebarGroup className="p-0">
+        <SidebarMenu>
+          {items.map((item) => {
+            const IconComponent = iconComponents[item.iconName]; // Get the component from the map
+            return (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  asChild
+                  tooltip={"item.title"}
+                  className={`${pathname.includes(item.url) && "bg-muted"}`}
+                >
+                  <Link
+                    href={item.url}
+                    className={`text-lg ${
+                      pathname.includes(item.url) && "font-bold"
+                    }`}
+                  >
+                    {IconComponent && <IconComponent className="text-lg" />}{" "}
+                    {/* Render the mapped component */}
+                    <span>{item.title}</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            );
+          })}
+        </SidebarMenu>
+      </SidebarGroup>
+    </SidebarProvider>
+  );
+};
 
-export {NavMain}
+export { NavMain };
